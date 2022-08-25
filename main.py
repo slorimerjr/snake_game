5b1bdf84af558f42dcc83ac845c1c099832a61b9
slorimerjr <slorimerjr@gmail.com>
2022-08-25 13:56:17 -0500

    simplified segment detection

--- main.py
+++ main.py
@@ -44,10 +44,8 @@ while game_is_on:
 
 
     #detect collision with tail
-    for segment in snake.segments:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
+    for segment in snake.segments[17:]:
+        if snake.head.distance(segment) < 10:
             game_is_on = False
             scoreboard.game_over()
 
