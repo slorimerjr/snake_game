c24310e10d514c0cad327e240bd814d021766333
slorimerjr <slorimerjr@gmail.com>
2022-08-25 13:12:59 -0500

    updated the init in the snake class

---
+++ scoreboard.py
@@ -0,0 +1,29 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Courier", 12, "normal")
+
+# scoreboard is going to be a turtle that knows how to keep track of the score and display it in our program.
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.goto(-20, 280)
+        self.color("white")
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER.", align=ALIGNMENT, font=("Courier", 20, "normal"))
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
+
